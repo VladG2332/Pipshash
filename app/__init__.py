52348540855ad8dc34e1333c5dc81eff2dbb1c02
VladG2332 <l21590080@sjuanrio.tecnm.mx>
2025-03-12 12:27:39 -0600

    INTEGRACION CRUD PIZZAS

--- app/__init__.py
+++ app/__init__.py
@@ -16,22 +16,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-<<<<<<< HEAD
 from app.models import Pizzas
 from app.models import Repartidores
 
-=======
-
-from app.models import Pizzas
->>>>>>> 8c2394e585512ffbae7e3daf199b74a751a4d4b1
 from app.routes.pizzas import pizzae_bp
 app.register_blueprint(pizzae_bp, url_prefix='/pipshash')
-<<<<<<< HEAD
-=======
-# Importar los modelos
-from app.models import Repartidores
-from app.models import Pizzas
->>>>>>> 8c2394e585512ffbae7e3daf199b74a751a4d4b1
 
 #Ruta Raiz
 @app.route('/')
